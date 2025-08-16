71896a1eec40f54e0b52cf30b87e2390f34c63d9
abdullah <elgendyabdala@gmail.com>
2025-08-16 13:25:31 +0300

    feat : add method getUsers

---
+++ src/components/users/Users.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+function Users() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://fakestoreapi.com/users")
+      .then(res => setUsers(res.data))
+      .catch(err => console.error(err));
+  }, []);
+
+  return (
+    <div>
+      <h2>Users</h2>
+      {users.map(u => (
+        <div key={u.id} >
+     
+          <Link to={`/users/${u.id}`}>
+          </Link>   
+          <p> ${u.username} </p>
+          <p>${u.email }</p>
+          <p>${u.phone}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Users;
